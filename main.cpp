4d8465f488925fffe97a138601d982310023c983
Longwei Su <longwei.su@gmail.com>
2022-05-31 23:31:17 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <climits>
+#include <iostream>
 
 int main(int argc, char **argv)
 {
@@ -10,9 +11,11 @@ int main(int argc, char **argv)
     char *dst = (char *)malloc(size1); // allocation size is 5 bytes
     char *src = (char *)malloc(size2); // allocation size is 10 bytes
 
+    memcpy(dst, src, size1);
+
     int a = INT_MAX;
     int b = INT_MAX;
     int c = a * b;
-
-    memcpy(dst, src, size1);
+    std::cout << c << std::endl;
+    return 0;
 }
