ad7d2aa25cc05c967a033451676c185163983359
Longwei Su <longwei.su@gmail.com>
2022-05-31 23:22:44 -0400

    trigger please

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <climits>
 
 int main(int argc, char **argv)
 {
@@ -8,6 +9,10 @@ int main(int argc, char **argv)
     int size2 = size1 + 5;
     char *dst = (char *)malloc(size1); // allocation size is 5 bytes
     char *src = (char *)malloc(size2); // allocation size is 10 bytes
+
+    int a = INT_MAX;
+    int b = INT_MAX;
+    int c = a * b;
 
     memcpy(dst, src, size1);
 }
